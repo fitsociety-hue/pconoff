0d4188e4713679d9a64127b5071fa85fcd4850b4
effect082 <your-email@example.com>
2026-06-25 17:49:10 +0900

    Update GAS URL to the latest deployment

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@
 // 2. 배포 -> 새 배포 -> 웹 앱 -> '강동어울림복지관의 모든 사용자' 로 설정
 // 3. 발급받은 URL을 아래에 입력하세요.
 const CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbysnHfBw7SUxTXb4OVrRHhwHLqeLW9InKpGn9znljKMyuq3LhZreNJ3-RtYHWrXDe_X/exec"
+    GAS_URL: "https://script.google.com/macros/s/AKfycbwA18GFOJq8zNbT1wdWuiaLc1vgivBmyVYXzkB8F_31ON3qnRnznSb0I_HHFVWAktjF/exec"
 };
