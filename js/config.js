76d857b7507796491a5f920ad9353bb846c9fbfa
effect082 <your-email@example.com>
2026-06-30 09:37:44 +0900

    Update deployment instruction in config.js to prevent CORS error

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Google Apps Script 배포 URL (웹앱 URL)
 // 1. backend/Code.gs를 Google Apps Script에 붙여넣기
-// 2. 배포 -> 새 배포 -> 웹 앱 -> '강동어울림복지관의 모든 사용자' 로 설정
+// 2. 배포 -> 새 배포 -> 웹 앱 -> 액세스 권한이 있는 사용자: '모든 사용자' 로 설정 (매우 중요! CORS 오류 방지)
 // 3. 발급받은 URL을 아래에 입력하세요.
 const CONFIG = {
     GAS_URL: "https://script.google.com/macros/s/AKfycbypVqiPUraYYDqFYkCfZws0vExl_3YPNUDqFusmfHFKfiEeH0kOzod2ac-ymfhfqbIx/exec"
