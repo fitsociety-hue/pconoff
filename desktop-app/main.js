5fa3481b8dc162b3fa52a6187227574f5e44beed
effect082 <your-email@example.com>
2026-07-03 17:08:48 +0900

    chore: update GAS_URL to new deployment and rebuild installer (v1.0.22)

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -7,7 +7,7 @@ const { exec, spawn } = require('child_process');
 const SHUTDOWN_CACHE_PATH = path.join(app.getPath('userData'), 'shutdown_cache.json');
 
 const CONFIG_PATH = path.join(app.getPath('userData'), 'user_config.json');
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxkvV93g2ssO6GoeUCunv4HgyIqRMbfRHfcYllaCdXhFT0-B-XmBwiqbUvIObabZm8/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzg3fi0v8CsbJxDaX35wKTyjW--PGnlIBwiNpT9GXj-w96u2y7d1JislgLBP66Kthos/exec";
 
 let tray = null;
 let mainWindow = null;
